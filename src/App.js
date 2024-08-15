c152f11f930b111bc952613daec69c0ea7c38e15
Tran Trung Kien <coinskus001@gmail.com>
2024-08-16 00:12:06 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import SongDetail from './components/Content/SongDetail';
-import SongList from './components/Content/SongList';
-import CreateSong from './components/Content/CreateSong';
-import './App.css';
-import SongEdit from './components/Content/SongEdit';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import React from 'react';
 
-function App() {
-  const [songs, setSongs] = useState([]);
-  const navigate = useNavigate();
-  const [ok, setOk] = useState(0);
-  useEffect(() => {
-    fetch('http://localhost:8080/song')
-      .then(response => response.json())
-      .then(data => setSongs(data))
-      .catch(error => console.error('Error fetching songs:', error));
-  }, [ok]);
-
-  const handleCreateClick = () => {
-    navigate('/create');
-  };
-
-  const handleDelete = (id) => {
-    setSongs(prevSongs => prevSongs.filter(song => song._id !== id));
-  };
-
-  
-
+const App = () => {
   return (
-    <div className="App">
-      <Header></Header>
-      <button onClick={handleCreateClick}>Create</button>
-      <Routes>
-        <Route path="/" element={<SongList songs={songs} onDelete={handleDelete} />} />
-        <Route path="/song/:id" element={<SongDetail songs={songs} />} />
-        <Route path="/create" element={<CreateSong />} />
-        <Route path="/songedit/:id" element={<SongEdit songs={songs}  setOk={setOk}/>} />
-      </Routes>
-      <Footer></Footer>
-    </div>
+      <>
+        <h1> Hello</h1>
+      </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'; // Correct import for React 18
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-
+import {Provider} from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  
+  
 );
