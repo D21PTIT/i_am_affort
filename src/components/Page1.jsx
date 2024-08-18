d51451d29ed130493c5f80f937c4108a5e0a8daa
Tran Trung Kien <coinskus001@gmail.com>
2024-08-18 18:52:17 +0700

    ok

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Page1.css";
 import { Switch } from "antd";
+import axios from "axios";
 
 function Page1() {
+  const [switches, setSwitches] = useState({
+    1: JSON.parse(localStorage.getItem("device-1")) || false,
+    2: JSON.parse(localStorage.getItem("device-2")) || false,
+    3: JSON.parse(localStorage.getItem("device-3")) || false,
+  });
+
   const onChange = (checked, switchNumber) => {
-    console.log(`Switch ${switchNumber}: ${checked ? 1 : 0}`);
+    let device = "";
+    let status = checked ? "on" : "off";
+
     if (switchNumber === 1) {
+      device = "fan";
     }
+    if (switchNumber === 2) {
+      device = "airc";
+    }
+    if (switchNumber === 3) {
+      device = "lightb";
+    }
+
+    // Lưu trạng thái vào Local Storage
+    localStorage.setItem(`device-${switchNumber}`, JSON.stringify(checked));
+
+    // Cập nhật trạng thái của switch trong state
+    setSwitches((prevSwitches) => ({
+      ...prevSwitches,
+      [switchNumber]: checked,
+    }));
+
+    const fetchDevices = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:8080/iot/createDevice",
+          {
+            tag: switchNumber,
+            name: device,
+            status: status,
+          }
+        );
+        console.log("Device status updated:", response.data);
+      } catch (err) {
+        console.error("Error updating device:", err);
+      }
+    };
+
+    fetchDevices();
   };
 
   return (
@@ -15,24 +58,34 @@ function Page1() {
       <div className="zone zone2">Zone 2</div>
       <div className="zone zone3">
         <div>
-          <Switch
-            defaultChecked
-            onChange={(checked) => onChange(checked, 1)}
-            style={{ marginRight: 8 }}
-          />
-          <Switch
-            defaultChecked
-            onChange={(checked) => onChange(checked, 2)}
-            style={{ marginRight: 8 }}
-          />
-          <Switch
-            defaultChecked
-            onChange={(checked) => onChange(checked, 3)}
-            style={{ marginRight: 8 }}
-          />
+          <div>
+            <p>Quạt</p>
+            <Switch
+              checked={switches[1]}
+              onChange={(checked) => onChange(checked, 1)}
+              style={{ marginRight: 8 }}
+            />
+          </div>
+
+          <div>
+            <p>Điều hòa</p>
+            <Switch
+              checked={switches[2]}
+              onChange={(checked) => onChange(checked, 2)}
+              style={{ marginRight: 8 }}
+            />
+          </div>
+
+          <div>
+            <p>Bóng điện</p>
+            <Switch
+              checked={switches[3]}
+              onChange={(checked) => onChange(checked, 3)}
+              style={{ marginRight: 8 }}
+            />
+          </div>
         </div>
       </div>
-      {/* <div className="zone zone4">Zone 4</div> */}
     </div>
   );
 }
--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import { Table } from "antd";
+import React, { useState } from "react";
 
 function Page2(props) {
+  const [data, setData] = useState([]);
+  const columns = [
+    {
+      title: "Độ ẩm",
+      dataIndex: "humidity",
+      key: "humidity",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Ánh sáng",
+      dataIndex: "light",
+      key: "light",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Nhiệt độ",
+      dataIndex: "temperature",
+      key: "temperature",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Thoi diem",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => <a>{text}</a>,
+    },
+  ];
   return (
     <div>
       <h2>Du lieu thuc</h2>
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 }
